0818095b1fd7bb88ec5fdb4a1ad41d6d9a447569
Trần Giang <hoanggiangtran239@gmail.com>
2026-04-02 15:30:28 +0700

    test pipeline

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cat")
-option_b = os.getenv('OPTION_B', "Dog")
+option_a = os.getenv('OPTION_A', "Cow")
+option_b = os.getenv('OPTION_B', "Bird")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
