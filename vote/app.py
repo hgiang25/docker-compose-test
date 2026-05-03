6dff2e67ef9b49e47fda3991069b1b38f9f9476b
Trần Giang <hoanggiangtran239@gmail.com>
2026-05-03 15:59:55 +0700

    chang vote app

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "GYANGGYANG")
-option_b = os.getenv('OPTION_B', "BOYYY")
+option_a = os.getenv('OPTION_A', "HGYang")
+option_b = os.getenv('OPTION_B', "GOGOOOO")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
