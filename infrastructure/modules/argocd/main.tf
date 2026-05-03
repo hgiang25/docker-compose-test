18a7838fa30737fbafcaed42509d029463e4037f
Trần Giang <hoanggiangtran239@gmail.com>
2026-05-03 13:56:04 +0700

    fix root app

--- infrastructure/modules/argocd/main.tf
+++ infrastructure/modules/argocd/main.tf
@@ -24,3 +24,41 @@ resource "helm_release" "argocd" {
   recreate_pods   = true
   cleanup_on_fail  = true # Nếu lỗi thì xóa làm lại sạch sẽ
 }
+
+# Đợi ArgoCD ổn định một chút rồi mới tạo Root App
+resource "time_sleep" "wait_for_argocd" {
+  depends_on = [helm_release.argocd]
+  create_duration = "30s"
+}
+
+resource "kubernetes_manifest" "root_app" {
+  depends_on = [time_sleep.wait_for_argocd]
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "root-app"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://github.com/hgiang25/k8s-gitops.git"
+        targetRevision = "main"
+        path           = "argocd"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "argocd"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        syncOptions = ["CreateNamespace=true"]
+      }
+    }
+  }
+}
