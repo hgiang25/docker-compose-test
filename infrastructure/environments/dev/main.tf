c176fefcf7134beb58f197f84b4fe5b73219af8b
Trần Giang <hoanggiangtran239@gmail.com>
2026-05-03 09:58:45 +0700

    provider helm

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -55,7 +55,7 @@ provider "kubernetes" {
 
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     token                  = data.aws_eks_cluster_auth.this.token
     cluster_ca_certificate = base64decode(module.eks.cluster_ca)
