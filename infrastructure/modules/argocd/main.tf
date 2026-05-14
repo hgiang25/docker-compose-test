4d8374ce6f98dd7cd534a7ff552954b9dbed8abb
Trần Giang <hoanggiangtran239@gmail.com>
2026-05-14 17:25:04 +0700

    metric server

--- infrastructure/modules/argocd/main.tf
+++ infrastructure/modules/argocd/main.tf
@@ -52,6 +52,16 @@ resource "helm_release" "metrics_server" {
   chart      = "metrics-server"
   version    = "3.12.2"
 
+  set {
+    name  = "args[0]"
+    value = "--kubelet-insecure-tls"
+  }
+
+  set {
+    name  = "args[1]"
+    value = "--kubelet-preferred-address-types=InternalIP"
+  }
+
   depends_on = [
     helm_release.argocd
   ]
