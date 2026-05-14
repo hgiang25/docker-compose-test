13739007fdd0eb0e02bc58dac2dc5e08fc2ede0f
Trần Giang <hoanggiangtran239@gmail.com>
2026-05-14 16:26:54 +0700

    argo rollout for dev

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -43,3 +43,18 @@ module "eks" {
 
   enviroment = "dev"
 }
+
+resource "helm_release" "argo_rollouts" {
+  name       = "argo-rollouts"
+  namespace  = "argo-rollouts"
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-rollouts"
+  version    = "2.37.6"
+
+  create_namespace = true
+
+  depends_on = [
+    module.eks
+  ]
+}
