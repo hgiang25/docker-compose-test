df6a6af6e989b7bfbd78be5e2f9760517bd1f699
Trần Giang <hoanggiangtran239@gmail.com>
2026-05-18 18:57:04 +0700

    export port 443

--- infrastructure/modules/addons/helm_release.tf
+++ infrastructure/modules/addons/helm_release.tf
@@ -126,4 +126,8 @@ resource "helm_release" "aws_lb_controller" {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
     value = module.aws_load_balancer_controller_irsa_role[0].iam_role_arn
   }
+  set {
+    name  = "enableServiceMutatorWebhook"
+    value = "true"
+  }
 }
