d700b8be7c66db8cee2df1d39991172adae22972
Trần Giang <hoanggiangtran239@gmail.com>
2026-05-23 14:14:35 +0700

    test access endpoint

--- infrastructure/modules/eks/main.tf
+++ infrastructure/modules/eks/main.tf
@@ -17,6 +17,10 @@ module "eks" {
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
+  cluster_endpoint_public_access_cidrs = [
+    "0.0.0.0/0"
+  ]
+
   # Bootstrap admin cluster creator
   #enable_cluster_creator_admin_permissions = true
 
